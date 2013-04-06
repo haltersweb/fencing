a082c4fa2d57694daf11aaac040dfdad585d9de3
haltersweb <haltersweb@gmail.com>
2013-04-06 13:34:02 -0400

    added notes link and array to hold fencer scoresheet data

--- source/js/script.js
+++ source/js/script.js
@@ -14,10 +14,21 @@ var FNC = FNC || (function () {
         boutAssignment : null
       };
     },
+    fencerData : [],
+    /*
+      {
+        fencerId : null,
+        vScore : 0,
+        tsScore : 0,
+        trScore : 0,
+        indicator : 0,
+        place : 0
+      }
+    */
     tourneyData : {}, //FNC.model
     getTournaments : function () { //FNC.model
-      var eventId = prompt("Enter the event number.");
-      $.getJSON("data/86450.json")
+      var eventId = prompt("Enter the event number (86450).");
+      $.getJSON("data/" + eventId + ".json")
         .done(function (data) {
           console.log("Test JSON Data: " + data.event.preregs[0].competitor.first_name + " " + data.event.preregs[0].competitor.last_name);
           FNC.model.tourneyData = data.event;
@@ -27,6 +38,21 @@ var FNC = FNC || (function () {
           var err = textStatus + ', ' + error;
           console.log("Request Failed: " + err);
         });
+    },
+    initializeFencingData : function () {
+      for (var i = 0; i < 8; i++) {
+        FNC.model.fencerData[i] = {
+          fencerId : null,
+          vScore : 0,
+          tsScore : 0,
+          trScore : 0,
+          indicator : 0,
+          place : 0
+        }
+      }
+    },
+    setFencingData_Id : function (index,id) {
+      FNC.model.fencerData[index] = id;
     },
     setBoutAssignment : function ($currentFencers) { //FNC.model
       //nullify FNC.model.boutAssignment at end of bout
@@ -79,15 +105,18 @@ var FNC = FNC || (function () {
       $('.draggable').draggable({ opacity: 0.7, helper: "clone", containment: "document", cursor: "pointer", revert: false, revertDuration: 300,
         start: function () {
           scorepadFencerPopulator = {
-            index : $(this).closest('li').attr('data-competitor-id'),
+            id : $(this).closest('li').attr('data-competitor-id'),
             text : $(this).text()
           };
         }
       });
       $('.drop-fencer').droppable({ hoverClass: "hover", tolerance: "pointer", accept: ".fencer",
         drop: function () {
-          $(this).attr('data-competitor-id', scorepadFencerPopulator.index);
+          var seed = $(this).attr('data-fencer-seed'),
+              id = scorepadFencerPopulator.id;
+          $(this).attr('data-competitor-id', id);
           $(this).text(scorepadFencerPopulator.text);
+          FNC.model.setFencingData_Id((seed - 1), id);
         }
       });
     },
@@ -191,6 +220,7 @@ var FNC = FNC || (function () {
 
 $(document).ready(function () {
   'use strict';
+  FNC.model.initializeFencingData();
   FNC.model.getObjects();
   FNC.model.getTournaments();
   FNC.view.getObjects();
